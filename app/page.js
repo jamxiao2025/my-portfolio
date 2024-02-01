cb9f7b81631dab6ff94eaf2176423faae560c432
jamxiao2025 <95207937+jamxiao2025@users.noreply.github.com>
2024-02-01 00:08:51 -0500

    updated blurb, added current proj. section, added social links

--- app/page.js
+++ app/page.js
@@ -8,15 +8,32 @@ export default function Home() {
        <text style={{fontSize: '30px'}}>James Xiao </text>
        <br></br>
        <br></br>
-       site status: <text style={{color:'orange'}}>under construction</text>
+       developing whatever itches my brain - <text style={{color:'orange'}}>powered by curiosity</text>
        <br></br><br></br><br></br>
+       I started my programming journey with full stack development. Many of my designs and code attempt to follow minimalism - there's something beautiful about getting more done with less. My most recent interests include LLM architecture, systems design, and honestly ANYTHING that can be programmed. My current goal is to actually CODE out my ideas instead of letting them stay as thoughts.  
        <br></br>
-       <br></br>
-       Currently experimenting with LLM frameworks for meaningful data indexing and retrieval. Interested in the role vector embeddings play within these processes.
-       <br></br>
+	<br></br>
+	<br></br>
+	Shoutout to Obsidian & Vim! 
        <br></br><br></br>
        <br></br><br></br>
-       Past projects include: 
+       Current Projects:
+	<br></br>
+	<br></br>
+	1. Video Codec - [<a 
+	target="_blank"
+	rel="noopener norefferrer"
+	style={{ color: 'blue'}}
+	href="https://github.com/jamxiao2025/Video-Compressor"
+	>media compression using SVD
+	</a>]
+	<br></br>
+	<text style={{color:'orange'}}>Ideally this project will allow me to learn more about image/video processing and parallel processing. I will also have the opportunity to learn a new stack (React + Django) and hopefully other devtools as an extension to the current project.</text>
+	<br></br>
+	<br></br>
+	<br></br>
+	<br></br> 
+       Past Projects: 
        <br></br>
        <br></br>
         1. BU Spark! Data Portal - [
@@ -44,10 +61,33 @@ export default function Home() {
          target="_blank"
          rel="noopener noreferrer"
          style={{ color: 'blue' }}
-        href="https://benmaimages.herokuapp.com"
+        href="https://github.com/jamxiao2025/Image-Gallery"
         >
           digitizing artistic vision 
         </a>]
+	<br></br>
+	<br></br>
+	<br></br>
+	<a
+	target="_blank"
+	rel="noopener noreferrer"
+	style={{ color: 'blue'}}
+	href="https://github.com/jamxiao2025"
+	>github</a>
+	<br></br>
+	<a
+	target="_blank"
+	rel="noopener noreferrer"
+	style={{ color: 'blue'}}
+	href="https://linkedin.com/in/jamxiao"
+	>linkedIn</a>
+	<br></br>
+	<a 
+	target="_blank"
+	rel="noopener noreferrer"
+	style={{ color: 'blue'}}
+	href="https://open.spotify.com/user/30s6wj6c2mgdxhi4j970mems1?si=lPvX9iTTTm-AOSIbNmxVMw"
+	>spotify"</a>
       </div>
     </main>
   )
